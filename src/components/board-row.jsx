61388826b3a2b6121ee977dc673364618a3e4651
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-10-26 16:15:49 -0500

    sufficiently styled tic tac toe board

--- src/components/board-row.jsx
+++ src/components/board-row.jsx
@@ -4,17 +4,17 @@ function BoardRow(props) {
   if (props.position === 'center') {
     return (
       <div className="row board-row">
-        <div className="cell col-4 text-center border-top border-bottom centered"><i className="fas fa-times x"></i></div>
-        <div className="cell col-4 text-center border centered"><i className="fas fa-times x"></i></div>
-        <div className="cell col-4 text-center border-top border-bottom centered"><i className="fas fa-times x"></i></div>
+        <div className="cell col-4 text-center b-top b-bottom centered"></div>
+        <div className="cell col-4 text-center my-border centered"></div>
+        <div className="cell col-4 text-center b-top b-bottom centered"></div>
       </div>
     );
   } else {
     return (
       <div className="row board-row">
-        <div className="cell col-4 text-center centered"><i className="far fa-circle o"></i></div>
-        <div className="cell col-4 text-center border-left border-right centered"><i className="far fa-circle o"></i></div>
-        <div className="cell col-4 text-center centered"><i className="far fa-circle o"></i></div>
+        <div className="cell col-4 text-center centered"></div>
+        <div className="cell col-4 text-center b-left b-right centered"></div>
+        <div className="cell col-4 text-center centered"></div>
       </div>
     );
   }
--- src/components/board.jsx
+++ src/components/board.jsx
@@ -4,7 +4,7 @@ import BoardRow from './board-row';
 function Board(props) {
   return (
     <>
-      <h1 className="text-center">Tic Tac Toe Board</h1>
+      <h1 className="text-center text-white">TIC TAC TOE</h1>
       <div className="container">
         <BoardRow position="top"/>
         <BoardRow position="center"/>
