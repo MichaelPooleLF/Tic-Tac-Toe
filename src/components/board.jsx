6a9b97f67f3927017f69b0346e0a1ac468ba0840
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-10-27 15:26:21 -0500

    user can make a move

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -12,10 +12,51 @@ class Board extends React.Component {
       ],
       isX: true
     };
+    this.handleClick = this.handleClick.bind(this);
+    this.setBoard = this.setBoard.bind(this);
+  }
+
+  setBoard(row, col, value) {
+    const updatedBoard = this.state.currentBoard.slice();
+    updatedBoard[row][col] = value;
+    this.setState({
+      currentBoard: updatedBoard,
+      isX: !this.state.isX
+    });
   }
 
   handleClick(event) {
-    console.log('click');
+    let currentMove = '';
+    this.state.isX ? currentMove = <i className="fas fa-times x"></i> : currentMove = <i className="far fa-circle o"></i>;
+    switch (event.target.id) {
+      case 'top-left':
+        this.setBoard(0, 0, currentMove);
+        break;
+      case 'top-center':
+        this.setBoard(0, 1, currentMove);
+        break;
+      case 'top-right':
+        this.setBoard(0, 2, currentMove);
+        break;
+      case 'center-left':
+        this.setBoard(1, 0, currentMove);
+        break;
+      case 'center':
+        this.setBoard(1, 1, currentMove);
+        break;
+      case 'center-right':
+        this.setBoard(1, 2, currentMove);
+        break;
+      case 'bottom-left':
+        this.setBoard(2, 0, currentMove);
+        break;
+      case 'bottom-center':
+        this.setBoard(2, 1, currentMove);
+        break;
+      case 'bottom-right':
+        this.setBoard(2, 2, currentMove);
+        break;
+    }
   }
 
   render() {
