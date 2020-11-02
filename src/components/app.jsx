a27bf49efdc02f557d7db0d59cc277bd2e1094de
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-11-02 17:00:31 -0600

    set state to proper initial state

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,7 +6,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      gameOver: true,
+      gameOver: false,
       gameStatus: ''
     };
     this.setGameStatus = this.setGameStatus.bind(this);
