b93ed2de9c42f1d1bda0d7cee77cbd08947877a7
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-11-02 16:39:40 -0600

    completed basic end game modal styling

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import Board from './board';
+import EndGameModal from './end-game-modal';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      gameOver: true,
+      gameStatus: ''
+    };
   }
 
   render() {
+    if (this.state.gameOver) {
+      return (
+        <>
+          <EndGameModal/>
+          <Board/>
+        </>
+      );
+    }
     return (
       <Board/>
     );
---
+++ src/components/end-game-modal.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function EndGameModal(props) {
+  return (
+    <div className="end-game-modal">
+      <div className="end-game-modal-content bg-dark">
+        <h1>Game Over</h1>
+        <h2>Player One Wins!</h2>
+      </div>
+    </div>
+  );
+}
+
+export default EndGameModal;
